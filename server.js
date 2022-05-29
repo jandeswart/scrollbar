cc5160eeffa8019dfcaaf4b1b2dab643d8881e1c
jandeswart <jan@wandelverhalen.net>
2022-05-29 10:15:10 +0200

    Synchronizing computers

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 /**
- * Start the server with: npm run dev
+ * Start the server with: * npm run dev *
  */
 
 const express = require("express");
-// const { dirname } = require("path");
 const app = express();
 const path = require("path");
 
+const PORT = 3000;
+
+/** The location of the static files: */
 app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = 3000;
-
+/** Run the server: */
 app.listen(PORT);
 
 console.log(`The app is running at port ${PORT}`);
